1813b34cf2f2eefd9ec844e532f5d5b3f805bca0
MINDEV\kin41 <kin412@naver.com>
2026-02-26 21:06:11 +0900

    섹션3. 예제 도메인 모델

--- data-jpa/src/main/java/study/data_jpa/entity/Member.java
+++ data-jpa/src/main/java/study/data_jpa/entity/Member.java
@@ -1,29 +1,49 @@
 package study.data_jpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    //lombok 의 @NoArgsConstructor 가 해줌
     //jpa는 프록시 방식으로 동작하기 때문에 기본 생성자가 있어야함.
-    protected Member() {}
+    //protected Member() {}
 
     public Member(String username) {
         this.username = username;
     }
 
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+
    /* public void changeUsername(String username) {
         this.username = username;
     }*/
+
+    //연관관계 메서드
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 
 }

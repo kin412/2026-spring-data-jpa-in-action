2adbecb07b6d75b6cd1e3649833e3d0c3d75bfc0
MINDEV\kin41 <kin412@naver.com>
2026-02-26 21:46:39 +0900

    섹션4. 공통 인터페이스 기능

--- data-jpa/src/main/java/study/data_jpa/DataJpaApplication.java
+++ data-jpa/src/main/java/study/data_jpa/DataJpaApplication.java
@@ -2,8 +2,11 @@ package study.data_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import study.data_jpa.entity.Member;
 
 @SpringBootApplication
+//@EnableJpaRepositories // @SpringBootApplication 이 이 어노테이션의 기능도 수행함
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
